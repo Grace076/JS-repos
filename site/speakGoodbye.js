83c0b918db336015026a3a220c44e77bfdc3b86e
Takie@03 <mbulahenitg02@gmail.com>
2021-02-02 22:33:43 +0200

    Thirdattempt

--- site/speakGoodbye.js
+++ site/speakGoodbye.js
@@ -2,6 +2,10 @@
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 (function(window){
 
@@ -17,7 +21,7 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak = function(name) {
+byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
--- site/speakHello.js
+++ site/speakHello.js
@@ -14,7 +14,7 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak = function(name) {
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
@@ -23,4 +23,4 @@ helloSpeaker.speak = function(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 window.helloSpeaker = helloSpeaker;
-})(window);
+})(window)
